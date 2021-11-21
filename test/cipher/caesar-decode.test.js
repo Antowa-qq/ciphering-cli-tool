7dc73167cbbb330870a1f926f1afaba72af55efe
Antowa-qq <anton-vinsss@mail.ru>
2021-11-21 22:02:58 +0300

    added caesar tests

---
+++ test/cipher/caesar-decode.test.js
@@ -0,0 +1,24 @@
+const { caesarDecode } = require('../../ciphers/caesar');
+
+describe('Cipher encrypt Caesar', () => {
+  test('Only latin letters ', () => {
+    let i = 'zhWAxh213!@!@#76623ыфвфы';
+    let o = 'ygVZwg213!@!@#76623ыфвфы';
+
+    expect(caesarDecode(i)).toBe(o);
+  });
+
+  test('keep register ', () => {
+    let i = 'abcABC';
+    let o = 'zabZAB';
+
+    expect(caesarDecode(i)).toBe(o);
+  });
+
+  test('Correct operation of the algorithm', () => {
+    let i = 'abcABC';
+    let o = 'zabZAB';
+
+    expect(caesarDecode(i)).toBe(o);
+  });
+});
